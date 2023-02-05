2c48f2009999e43ab994f0b86e4da8186d96beff
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2023-02-05 19:00:57 +0530

    changes in layout width just a small

--- src/Component/HSteppers/HSteppers.jsx
+++ src/Component/HSteppers/HSteppers.jsx
@@ -22,10 +22,10 @@ const HSteppers = ({ employes, setemployes }) => {
           <tr className=" lg:flex flex-col lg:flex-row border-b lg:border-grey-light min-w-fit hidden">
             {employeStage.map((step, stepIdx) => (
               <td
-                className="relative py-3 w-full  lg:py-4 first:pl-8 font-semibold first:xl:w-10/12 first:w-full text-xl"
+                className="relative py-3 w-1/6 flex justify-start    lg:py-4 first:pl-8 font-semibold  first:2xl:w-1/6 first:xl:w-1/4  first:lg:w-1/3 text-xl"
                 key={stepIdx}
               >
-                <div className="first:min-w-max 2xl:w-48 xl:w-36">
+                <div className="first:min-w-max 2xl:w-32 xl:w-36">
                   {step.name}
                 </div>
               </td>
